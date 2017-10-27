8edabf421da4ae685c75f83f61fdb09c2deb97ad
patatahooligan <patatahooligan@hotmail.com>
2017-10-27 19:25:07 +0300

    Implement methods that save particle to binary file

--- thesis/savestate.cpp
+++ thesis/savestate.cpp
@@ -1,1 +1,28 @@
 #include "stdafx.h"
+
+#include "savestate.h"
+
+std::ostream& operator<<(std::ostream& out, Vec3f vec) {
+	out << vec.x << vec.y << vec.z;
+	return out;
+}
+
+std::ostream& operator<<(std::ostream& out, Particle& particle) {
+	out <<
+		particle.position <<
+		particle.velocity <<
+		particle.acceleration <<
+		particle.temperature <<
+		particle.temperature_derivative <<
+		particle.density <<
+		particle.density_derivative <<
+		particle.temperature <<
+		particle.temperature_derivative <<
+		particle.viscocity;
+	return out;
+}
+
+void SaveState::save(particlearray &data) {
+	for (auto& particle : data)
+		output_file << particle;
+}
--- thesis/savestate.h
+++ thesis/savestate.h
@@ -4,20 +4,16 @@
 
 #include "particle.h"
 
-std::ostream& operator<<(std::ostream& out, Particle& particle) {
-	// TODO
-	return out;
-}
-
 class SaveState {
 	public:
 		SaveState(std::string output_filename) :
-			output_file(output_filename, std::ios_base::binary) {}
-
-		void save(particlearray &data) {
-			for (auto& particle : data)
-				output_file << particle;
+			output_file(output_filename, std::ios_base::binary) {
+			// Write a header that indicates the format and the version number
+			// This way if the format changes, backwards compatibility can be achieved.
+			output_file << "SaveState" << unsigned int{ 1 };
 		}
+
+		void save(particlearray &data);
 
 	private:
 		std::ofstream output_file;
