78dba86d9e4256a0d9647015861b039eed7d445e
patatahooligan <patatahooligan@hotmail.com>
2017-10-27 19:40:57 +0300

    Make SaveState throw if it cannot open the file and delete default constructor

--- thesis/savestate.h
+++ thesis/savestate.h
@@ -6,10 +6,16 @@
 
 class SaveState {
 	public:
+		// Disallow construction without a file to open
+		SaveState() = delete;
+
 		SaveState(std::string output_filename) :
 			output_file(output_filename, std::ios_base::binary) {
 			// Write a header that indicates the format and the version number
 			// This way if the format changes, backwards compatibility can be achieved.
+			if (!output_file.is_open)
+				throw std::runtime_error(std::string("Could not open file ") + output_filename);
+
 			output_file << "SaveState" << unsigned int{ 1 };
 		}
 
