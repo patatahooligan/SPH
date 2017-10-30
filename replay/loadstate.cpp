13d448e6bcab87dd02b6a5bccfa7882a5c65580d
patatahooligan <patatahooligan@hotmail.com>
2017-10-30 21:13:50 +0200

    Add replay project that reads a savestate file and plays it back

---
+++ replay/loadstate.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+
+#include <iomanip>
+
+#include "loadstate.h"
+
+LoadState::LoadState(std::string input_filename) :
+	input_file(input_filename, std::ios_base::binary)
+{
+	if (!input_file.is_open)
+		throw std::runtime_error("Cannot open file");
+
+	// Verify the format of the file
+	unsigned int version;
+	std::string header;
+	const std::string target("SaveState");
+
+	input_file >> std::setw(target.length()) >> header;
+	input_file >> version;
+	if (header.compare(target) || version != 1)
+		throw std::runtime_error("Invalid input file format");
+}
+
+std::istream& operator>>(std::istream &in, Particle &particle) {
+	in >>
+		particle.position >>
+		particle.velocity >>
+		particle.acceleration >>
+		particle.temperature >>
+		particle.temperature_derivative >>
+		particle.density >>
+		particle.density_derivative >>
+		particle.temperature >>
+		particle.temperature_derivative >>
+		particle.viscocity;
+	return in;
+}
+
+void LoadState::load(particlearray& target_array) {
+	for (auto& particle : target_array) {
+		input_file >> particle;
+	}
+}
---
+++ replay/loadstate.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <fstream>
+#include <vector>
+
+#include "particle.h"
+
+class LoadState {
+	public:
+		// Disallow construction without a file to open
+		LoadState() = delete;
+
+		LoadState(std::string input_filename);
+
+		void load(particlearray& target_array);
+
+		particlearray load() {
+			// Same functionality as the other load, to offer a choice on return method
+			particlearray target_array;
+			load(target_array);
+			return target_array;
+		}
+
+	private:
+		std::ifstream input_file;
+};
