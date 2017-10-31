40c9c3db8fecec9e3f382bf64836cccfb8f1d074
patatahooligan <patatahooligan@hotmail.com>
2017-10-31 19:25:08 +0200

    Add operator>>(std::istream &in, Vec3f vec)
    Also bump operator>>(std::istream &in, Particle &particle) to the top of the file. This separates the two istream helper functions from the class methods of LoadState.

--- replay/loadstate.cpp
+++ replay/loadstate.cpp
@@ -4,21 +4,9 @@
 
 #include "loadstate.h"
 
-LoadState::LoadState(std::string input_filename) :
-	input_file(input_filename, std::ios_base::binary)
-{
-	if (!input_file.is_open())
-		throw std::runtime_error("Cannot open file");
-
-	// Verify the format of the file
-	unsigned int version;
-	std::string header;
-	const std::string target("SaveState");
-
-	input_file >> std::setw(target.length()) >> header;
-	input_file >> version;
-	if (header.compare(target) || version != 1)
-		throw std::runtime_error("Invalid input file format");
+std::istream& operator>>(std::istream &in, Vec3f vec) {
+	in >> vec.x >> vec.y >> vec.z;
+	return in;
 }
 
 std::istream& operator>>(std::istream &in, Particle &particle) {
@@ -34,6 +22,23 @@ std::istream& operator>>(std::istream &in, Particle &particle) {
 		particle.temperature_derivative >>
 		particle.viscocity;
 	return in;
+}
+
+LoadState::LoadState(std::string input_filename) :
+	input_file(input_filename, std::ios_base::binary)
+{
+	if (!input_file.is_open())
+		throw std::runtime_error("Cannot open file");
+
+	// Verify the format of the file
+	unsigned int version;
+	std::string header;
+	const std::string target("SaveState");
+
+	input_file >> std::setw(target.length()) >> header;
+	input_file >> version;
+	if (header.compare(target) || version != 1)
+		throw std::runtime_error("Invalid input file format");
 }
 
 void LoadState::load(particlearray& target_array) {
