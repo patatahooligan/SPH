e5159d9531c1c618b0cc1469129e4eba17a34928
patatahooligan <patatahooligan@hotmail.com>
2017-11-03 20:27:56 +0200

    Add missing const qualifiers to SaveState functions

--- thesis/savestate.cpp
+++ thesis/savestate.cpp
@@ -2,12 +2,12 @@
 
 #include "savestate.h"
 
-std::ostream& operator<<(std::ostream& out, Vec3f vec) {
+std::ostream& operator<<(std::ostream& out, const Vec3f &vec) {
 	out << vec.x << vec.y << vec.z;
 	return out;
 }
 
-std::ostream& operator<<(std::ostream& out, Particle& particle) {
+std::ostream& operator<<(std::ostream& out, const Particle& particle) {
 	out <<
 		particle.position <<
 		particle.velocity <<
@@ -22,7 +22,7 @@ std::ostream& operator<<(std::ostream& out, Particle& particle) {
 	return out;
 }
 
-void SaveState::save(particlearray &data) {
+void SaveState::save(const particlearray &data) {
 	for (auto& particle : data)
 		output_file << particle;
 }
--- thesis/savestate.h
+++ thesis/savestate.h
@@ -19,7 +19,7 @@ class SaveState {
 			output_file << std::string("SaveState") << unsigned int{ 1 };
 		}
 
-		void save(particlearray &data);
+		void save(const particlearray &data);
 
 	private:
 		std::ofstream output_file;
