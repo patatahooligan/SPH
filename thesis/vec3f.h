84664dccba83e0e97b29df020cb6b0033e7c317e
patatahooligan <patatahooligan@hotmail.com>
2017-11-18 01:45:38 +0200

    Move operators into vec3f.h to allow inlining

--- thesis/vec3f.h
+++ thesis/vec3f.h
@@ -10,23 +10,52 @@ class Vec3f {
 
 		Vec3f& operator=(const Vec3f &other) = default;
 
-		Vec3f& operator+=(const Vec3f &other);
+		Vec3f& operator+=(const Vec3f &other) {
+			x += other.x;
+			y += other.y;
+			z += other.z;
+			return *this;
+		}
 
-		Vec3f& operator-=(const Vec3f &other);
+		Vec3f& operator-=(const Vec3f &other) {
+			x -= other.x;
+			y -= other.y;
+			z -= other.z;
+			return *this;
+		}
 
-		float dot_product(const Vec3f &other) const;
+		float dot_product(const Vec3f &other) const {
+			return x*other.x + y * other.y + z * other.z;
+		}
 
-		float length_squared() const;
+		float length_squared() const {
+			return x*x + y*y + z*z;
+		}
 		
-		float length() const;
+		float length() const {
+			return sqrt(length_squared());
+		}
 };
 
-Vec3f operator+(Vec3f v1, const Vec3f &v2);
+Vec3f operator+(Vec3f v1, const Vec3f &v2) {
+	return v1 += v2;
+}
 
-Vec3f operator-(Vec3f v1, const Vec3f &v2);
+Vec3f operator-(Vec3f v1, const Vec3f &v2) {
+	return v1 -= v2;
+}
 
-Vec3f operator*(const Vec3f &v, const float c);
+Vec3f operator*(Vec3f v, const float c) {
+	return v *= c;
+}
 
-Vec3f operator*(const float c, const Vec3f &v);
+Vec3f operator*(const float c, Vec3f v) {
+	return v *= c;
+}
 
-Vec3f operator/(const Vec3f &v, const float c);
+Vec3f operator/(const Vec3f &v, const float c) {
+	return Vec3f(
+		v.x / c,
+		v.y / c,
+		v.z / c);
+}
