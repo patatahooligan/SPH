97ed571917d596ccc6fa152393c27cab7b8dcac2
patatahooligan <patatahooligan@hotmail.com>
2017-12-10 15:54:11 +0200

    Code cleanup & removal of obsolete debug features

--- thesis/kdtree.h
+++ thesis/kdtree.h
@@ -12,24 +12,14 @@ class ParticleAdaptor {
 
 	private:
 		const particlearray &particles;
-		const particlearray *particles_p;
 
 	public:
 		ParticleAdaptor() = delete;		// Must construct with reference to particles
 		ParticleAdaptor(const particlearray &target_array) :
-			particles(target_array),
-			particles_p(&target_array)
+			particles(target_array)
 			{};
 
 		inline size_t kdtree_get_point_count() const { return num_of_particles; }
-
-		inline float kdtree_distance(const float *p1, const size_t idx_p2, size_t size) const {
-			float
-				dx = p1[0] - particles[idx_p2].position.x,
-				dy = p1[1] - particles[idx_p2].position.y,
-				dz = p1[2] - particles[idx_p2].position.z;
-			return dx*dx + dy*dy + dz*dz;
-		}
 
 		inline float kdtree_get_pt(const size_t idx, int dim) const {
 			// TODO: after testing this, consider removing the invalid argument check
@@ -40,8 +30,6 @@ class ParticleAdaptor {
 					return particles[idx].position.y;
 				case 2:
 					return particles[idx].position.z;
-				default:
-					throw std::invalid_argument(std::string("Invalid dimension request ") + std::to_string(dim));
 			}
 		}
 
