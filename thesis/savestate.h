b80aa5b20aad04b5684e0babd02feff5fa01b38a
patatahooligan <patatahooligan@hotmail.com>
2018-01-15 15:04:20 +0200

    Change SaveState to discard any previous content when opening a file

--- thesis/savestate.h
+++ thesis/savestate.h
@@ -12,9 +12,7 @@ class SaveState {
 		SaveState(SaveState&&) = default;
 
 		SaveState(std::string output_filename) :
-			output_file(output_filename, std::ios_base::binary) {
-			// Write a header that indicates the format and the version number
-			// This way if the format changes, backwards compatibility can be achieved.
+			output_file(output_filename, std::ofstream::binary | std::ofstream::trunc) {
 			if (!output_file.is_open())
 				throw std::runtime_error(std::string("Could not open file ") + output_filename);
 		}
