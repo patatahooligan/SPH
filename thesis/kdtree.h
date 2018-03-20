6270e8f1cc63eeb5c4c10529a8f64003dd06e479
patatahooligan <filwn.oikonomou@gmail.com>
2018-03-20 18:20:51 +0200

    Add directive to supress a warning for non-returning path of execution
    ParticleAdaptor::kd_tree_get_pt() cannot validate its argument because it must have minimal overhead. This issues a warning in MSVC which we do not need as it is working as intended. An assert statement has been added to catch errors that would result in undefined behavior.

--- thesis/kdtree.h
+++ thesis/kdtree.h
@@ -21,7 +21,12 @@ class ParticleAdaptor {
 
 		inline size_t kdtree_get_point_count() const { return num_of_particles; }
 
+		// This function should never be called with an invalid dim
+		// Because it is used in a heavy computational loop, we have to omit
+		// any error checking and rely on proper calling
+		#pragma warning(suppress: 4715)
 		inline float kdtree_get_pt(const size_t idx, int dim) const {
+			assert(dim >= 0 && dim <= 2);
 			switch (dim) {
 				case 0:
 					return particles[idx].position.x;
