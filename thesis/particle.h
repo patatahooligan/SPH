f7a45b5612af06ef2dec9943950f1833a5406bb7
patatahooligan <filwn.oikonomou@gmail.com>
2018-03-22 17:29:20 +0200

    Change Particle definition to equivalent struct
    Particle is just a holder of data. Changing it to struct and removing member functions clearly communicates its nature.

--- thesis/particle.h
+++ thesis/particle.h
@@ -6,18 +6,10 @@
 #include "constants.h"
 #include "vec3f.h"
 
-class Particle {
+struct Particle {
 	// A single particle of the SPH simulation
-public:
 	Vec3f position, velocity, velocity_half, acceleration;
 	float density;
-
-	// Would have been generated implicitly anyway, but explicit declaration is preferred
-	Particle() = default;
-	Particle(const Particle&) = default;
-	Particle(Particle&& other) = default;
-	Particle& operator=(const Particle& other) = default;
-	Particle& operator=(Particle&& other) = default;
 };
 
 // This is used across translation units. Defining it here enforces consistency and facilitates changes.
