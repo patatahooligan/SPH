58c0ac580a0815ff95b6b9b0da45c80095b51d2f
patatahooligan <filwn.oikonomou@gmail.com>
2018-04-17 18:26:29 +0300

    Change binary file to only contain position of Particles
    While it is useful to hold other data for debugging reasons, Particle is continuously changing while the physics are being rewritten. When the form is final, the binary format can be enhanced to contain other data as well.

--- thesis/savestate.cpp
+++ thesis/savestate.cpp
@@ -9,8 +9,5 @@ void SaveState::save(const ParticleContainer &data) {
 
 void SaveState::save_particle(const Particle& particle) {
 	output_file
-		.write(reinterpret_cast<const char*>(&particle.position), sizeof(particle.position))
-		.write(reinterpret_cast<const char*>(&particle.velocity), sizeof(particle.velocity))
-		.write(reinterpret_cast<const char*>(&particle.acceleration), sizeof(particle.acceleration))
-		.write(reinterpret_cast<const char*>(&particle.density), sizeof(particle.density));
+		.write(reinterpret_cast<const char*>(&particle.position), sizeof(particle.position));
 }
