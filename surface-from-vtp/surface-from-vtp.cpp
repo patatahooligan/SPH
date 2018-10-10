957d0fe856aba988afc1a4fcf1af99b6f3489ea9
patatahooligan <filwn.oikonomou@gmail.com>
2018-10-10 15:39:03 +0300

    Allow surface-from-vtp to accept a starting index for resuming paused jobs

--- surface-from-vtp/surface-from-vtp.cpp
+++ surface-from-vtp/surface-from-vtp.cpp
@@ -3,12 +3,12 @@
 #include "fileIO.h"
 
 int main(int argc, char* argv[]) {
-	if (argc != 3)
+	if (argc < 3)
 		return EXIT_FAILURE;
 
 	constexpr float h = 0.0346410161513775f;
 
-	int i = 0;
+	int i = argc >= 3 ? std::stoi(argv[3]) : 0;
 	const std::string
 		input_prefix = argv[1],
 		output_prefix = argv[2];
