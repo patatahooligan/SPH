ddce3ffc23649a43620d2d5777cf69de353b1759
patatahooligan <filwn.oikonomou@gmail.com>
2018-10-15 15:51:19 +0300

    Fix a bug where springs were created with both ends on the same particle

--- thesis/physics.cpp
+++ thesis/physics.cpp
@@ -291,7 +291,7 @@ void ParticleSystem::generate_mass_spring_damper() {
 		for (const auto& index_pair : neighbor_indices) {
 			for (int j = index_pair.first; j < index_pair.second; ++j) {
 				const float distance = (particles[i].position - particles[j].position).length();
-				if (distance <= case_def.spring.max_length) {
+				if (distance <= case_def.spring.max_length && distance > 0) {
 					MassSpringDamper msp;
 					msp.particle_indices = { i, j };
 					msp.resting_length = distance;
@@ -311,7 +311,7 @@ void ParticleSystem::generate_mass_spring_damper() {
 		for (const auto& index_pair : neighbor_indices) {
 			for (int j = index_pair.first; j < index_pair.second; ++j) {
 				const float distance = (particles[i].position - particles[j].position).length();
-				if (distance <= case_def.spring.max_length) {
+				if (distance <= case_def.spring.max_length && distance > 0) {
 					MassSpringDamper msp;
 					msp.particle_indices = { i, j };
 					msp.resting_length = distance;
