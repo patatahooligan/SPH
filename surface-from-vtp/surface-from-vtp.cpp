7c0c45409fa5128a9948f3a4153e4b9767df23c7
patatahooligan <filwn.oikonomou@gmail.com>
2019-01-15 17:33:19 +0200

    Output progress when creating surface and properly do nothing for empty input

--- surface-from-vtp/surface-from-vtp.cpp
+++ surface-from-vtp/surface-from-vtp.cpp
@@ -6,18 +6,22 @@ int main(int argc, char* argv[]) {
 	if (argc < 3)
 		return EXIT_FAILURE;
 
-	constexpr float resolution = 0.007;
+	constexpr float resolution = 0.006;
 	const std::string
 		input_prefix = argv[1],
 		output_prefix = argv[2];
 
 	const int	begin_index = argc >= 4 ? std::stoi(argv[3]) : 0;
 	int end_index = begin_index;
-	while (std::filesystem::exists(input_prefix + std::to_string(++end_index) + ".vtp"));
+	while (std::filesystem::exists(input_prefix + std::to_string(end_index) + ".vtp"))
+		++end_index;
 
 	for (int i = begin_index; i < end_index; ++i) {
 		const auto polydata = polydata_from_vtp(input_prefix + std::to_string(i) + ".vtp");
 		const auto surface = surface_from_polydata(polydata, resolution);
-		save_surface_to_vtp(surface, output_prefix + std::to_string(i));
+		const auto output_filename = output_prefix + std::to_string(i);
+		save_surface_to_vtp(surface, output_filename);
+
+		std::cout << "Saved output to " << output_filename << '\n';
 	}
 }
